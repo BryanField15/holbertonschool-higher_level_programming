e24d67ff8a199c93f72e49fd861d529c46e5ad31
BryanField15 <6367@holbertonstudents.com>
2023-05-29 18:35:24 -0700

    silly solution

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-words = str.split()
-middle_words = ' '.join(words[5:8])
-first_word = words[0]
-str = f"{middle_words} with {first_word}"
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
