842ad3efae070f747fdf30ce018997e55b790553
BryanField15 <6367@holbertonstudents.com>
2023-05-31 21:01:32 -0700

    fixed order of conditional statements

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -12,11 +12,11 @@ str1 = 'Last digit of'
 str2 = 'and is less than 6 and not 0'
 str3 = 'and is greater than 5'
 
-if (last_digit < 6):
-    print("{} {} is {} {}".format(str1, number, last_digit, str2))
-
-elif (last_digit > 5):
+if (last_digit > 5):
     print("{} {} is {} {}".format(str1, number, last_digit, str3))
 
-else:
+elif (last_digit == 0):
     print("{} {} is {} and is 0".format(str1, number, last_digit))
+
+else:
+    print("{} {} is {} {}".format(str1, number, last_digit, str2))
