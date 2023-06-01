604d977b31e21933a03edfba04cd5b4ae5273737
BryanField15 <6367@holbertonstudents.com>
2023-05-31 20:39:59 -0700

    blah blah blah

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ str2 = 'and is less than 6 and not 0'
 str3 = 'and is greater than 5'
 
 if (0 < last_digit < 6):
-    print("{} {:d} is {:d} {}".format(str1, number, last_digit, str2))
+    print("{} {} is {} {}".format(str1, number, last_digit, str2))
 
 elif (last_digit > 5):
-    print("{} {:d} is {:d} {}".format(str1, number, last_digit, str3))
+    print("{} {} is {} {}".format(str1, number, last_digit, str3))
 
 else:
-    print("{} {:d} is {:d} and is 0".format(str1, number, last_digit))
+    print("{} {} is {} and is 0".format(str1, number, last_digit))
