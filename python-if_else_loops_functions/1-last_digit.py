25cd7b737dc18ba02c0369d77e658015c8fcd4e7
BryanField15 <6367@holbertonstudents.com>
2023-05-31 20:54:34 -0700

    fixed diplay of negative last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,17 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10
+
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = -1 * ((-1 * number) % 10)
+
 str1 = 'Last digit of'
 str2 = 'and is less than 6 and not 0'
 str3 = 'and is greater than 5'
 
-if (0 < last_digit < 6):
+if (last_digit < 6):
     print("{} {} is {} {}".format(str1, number, last_digit, str2))
 
 elif (last_digit > 5):
