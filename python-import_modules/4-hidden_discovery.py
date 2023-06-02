05aa9388b8f232e132b428a8f9c20e035010adef
BryanField15 <6367@holbertonstudents.com>
2023-06-02 03:33:38 -0700

    pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,13 @@
 
 import hidden_4
 
+
 def print_names():
     names = dir(hidden_4)
     for target_name in names:
         if target_name[:2] != "__":
             print(target_name)
 
+
 if __name__ == "__main__":
-   print_names()
+    print_names()
