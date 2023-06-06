2a1c62b75a1bc5fdc952a34b6dd38680cfa91b75
BryanField15 <6367@holbertonstudents.com>
2023-06-06 04:20:17 -0700

    meet requirements

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print(' '.join(map(str, i)))
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=' ')
+        print()
