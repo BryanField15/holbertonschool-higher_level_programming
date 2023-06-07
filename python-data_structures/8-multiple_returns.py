3ec895241f55deb33b28c27e70cef07820f73a44
BryanField15 <6367@holbertonstudents.com>
2023-06-07 03:32:03 -0700

    task 8 solution

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+        first = sentence[0]
+        if sentence is None:
+            first = None
+        length = len(sentence)
+        return length, first
