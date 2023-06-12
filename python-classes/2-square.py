e79f62b3299bba8bf9091bee521d6a181878f6fc
BryanField15 <6367@holbertonstudents.com>
2023-06-11 22:31:23 -0700

    task 2 solution

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+'''Define the class called Square'''
+
+
+class Square:
+    '''A simple definition of a square'''
+
+    def __init__(self, size=0):
+        '''Initialize Square instance with size'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
