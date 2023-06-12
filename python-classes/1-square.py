e2f6b63acb9b6d9bdf164a2b445b068c8ec3d13d
BryanField15 <6367@holbertonstudents.com>
2023-06-11 22:07:10 -0700

    task 1 solution

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+'''Define the class called Square'''
+
+
+class Square:
+    '''A simple definition of a square'''
+
+    def __init__(self, size):
+        self.__size = size
