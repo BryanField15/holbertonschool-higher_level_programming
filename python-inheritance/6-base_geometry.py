cc0295328cb854d08c3d18be3b4d1a5d5716832e
BryanField15 <6367@holbertonstudents.com>
2023-06-19 23:32:54 -0700

    task 6 solution

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module with empty class"""
+
+
+class BaseGeometry():
+    """Function that is empty"""
+    def area(self):
+        raise Exception("area() is not implemented")
