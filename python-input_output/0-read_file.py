5b2fe8052b9257cebe4a1d48f4b3df4bf9d96503
BryanField15 <6367@holbertonstudents.com>
2023-06-22 16:33:13 -0700

    added rstrip

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Function reads a text file (UTF8), prints to stdout"""
     with open(filename) as f:
         contents = f.read()
-    print(contents)
+    print(contents.rstrip())
