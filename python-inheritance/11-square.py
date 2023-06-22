cc6c789974c1b8e966eecf1a14efb9ffc6e5d05e
BryanField15 <6367@holbertonstudents.com>
2023-06-21 21:40:39 -0700

    task 11 solution

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module with class square that inherits from Rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class that represents a square"""
+    def __init__(self, size):
+        """Initialize the square's dimensions"""
+        BaseGeometry().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Method to calculate area"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """String representation of the Square object"""
+        return f"[Square] {self.__size}/{self.__size}"
