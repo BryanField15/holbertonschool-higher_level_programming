3da985816d5ed5a9a8ef8dc485152865215ac966
BryanField15 <6367@holbertonstudents.com>
2023-06-21 19:31:52 -0700

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,3 @@ class MyList(list):
     def print_sorted(self):
         """Method prints the list, sorted ascedning"""
         print(sorted(self))
-
-    def __str__(self):
-        return super().__str__()
