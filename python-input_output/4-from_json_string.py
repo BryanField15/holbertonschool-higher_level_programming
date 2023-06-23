cdf54cd281497b7d54df1d5dbccb1dd43052eb14
BryanField15 <6367@holbertonstudents.com>
2023-06-22 19:47:33 -0700

    task 4 solution

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module with one function"""
+
+
+import json
+
+def from_json_string(my_str):
+    """Function returns an object represented by a JSON string:"""
+    return json.loads(my_str)
