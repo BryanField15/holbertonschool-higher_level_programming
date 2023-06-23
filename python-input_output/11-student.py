ed32532b7116437fb255a2cd9903f2a3c2feddf8
BryanField15 <6367@holbertonstudents.com>
2023-06-23 03:43:55 -0700

    task 11 solution

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""""Class Student that describes a student"""
+
+
+class Student:
+    """Simple attempt to model a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize the student attibutes
+        :param first_name: The student's first name
+        :param last_name: The student's last name
+        :param age: The student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of a Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
