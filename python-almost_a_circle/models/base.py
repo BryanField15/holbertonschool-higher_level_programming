82acb63ca8920d62dcafa21d5087eae45bc132ba
BryanField15 <6367@holbertonstudents.com>
2023-06-25 22:34:22 -0700

    pycodestyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,12 +4,12 @@
 
 class Base:
     """Class that manages id attribute of future classes"""
-    __nb_objects = 0 #private class attribute
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialize class attibutes"""
         if id is not None:
-           self.id = id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
