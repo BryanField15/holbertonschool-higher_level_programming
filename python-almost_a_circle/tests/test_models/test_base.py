4ca41ebb7a2d103506b5da1c4684ee8e5c757b9a
BryanField15 <6367@holbertonstudents.com>
2023-06-28 02:14:16 -0700

    test the test

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Unit test for class Base"""
+import unittest
+from models.base import Base
+
+
+class TestBaseClass(unittest.TestCase):
+    """Tests for Base class"""
+
+    def test_auto_id_assignment(self):
+        """Test that Base assigns a unique ID"""
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
