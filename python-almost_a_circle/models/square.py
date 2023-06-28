bfb7d5847bad8ea3be94a561600627bbb1a59eef
BryanField15 <6367@holbertonstudents.com>
2023-06-27 18:59:04 -0700

    task 9 solution

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module with class square"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class that describes a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Insitializes the attributes of the square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns a formatted string"""
+        return (
+            f"[Square] ({self.id}) {self.x}/{self.y} - "
+            f"{self.width}"
+        )
