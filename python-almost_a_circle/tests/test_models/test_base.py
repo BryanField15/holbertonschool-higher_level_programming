14d9675a432ab41105848ea0d07ee6b6ab24ea3b
BryanField15 <6367@holbertonstudents.com>
2023-06-28 03:04:44 -0700

    try again

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,14 +2,12 @@ import unittest
 from models.base import Base
 
 class TestBase(unittest.TestCase):
-    """
-    Tests for Base class
-    """
+    """Tests for Base class"""
 
-    def test_auto_assign_id(self):
-        """
-        Test that Base assigns a unique ID
-        """
+    def test_auto_id_assignment(self):
+        """Test that Base assigns a unique ID"""
         base1 = Base()
         base2 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
         self.assertNotEqual(base1.id, base2.id)
