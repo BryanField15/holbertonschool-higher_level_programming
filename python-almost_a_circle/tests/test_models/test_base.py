cb51166154519ac70a6f8d57c4ffc2f4648eea6c
BryanField15 <6367@holbertonstudents.com>
2023-06-28 04:16:14 -0700

    two tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,8 +6,16 @@ class TestBase(unittest.TestCase):
 
     def test_auto_id_assignment(self):
         """Test that Base assigns a unique ID"""
-        base1 = Base()
-        base2 = Base()
-        self.assertEqual(base1.id, 1)
-        self.assertEqual(base2.id, 2)
-        self.assertNotEqual(base1.id, base2.id)
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertNotEqual(b1.id, b2.id)
+
+    def test_auto_id_assignment_plus_one(self):
+        """Test that checks  ID's are sequential"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id + 1, b2.id)
+        self.assertEqual(b2.id + 1, b3.id)
