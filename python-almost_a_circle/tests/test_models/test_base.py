a3a64881cfcdae98ae330c20db2b91b5c8398761
BryanField15 <6367@holbertonstudents.com>
2023-06-28 02:49:55 -0700

    spaces and comments

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,12 +1,15 @@
 import unittest
 from models.base import Base
 
-
 class TestBase(unittest.TestCase):
-    """Tests for Base class"""
+    """
+    Tests for Base class
+    """
 
     def test_auto_id_assignment(self):
-        """Test that Base assigns a unique ID"""
+        """
+        Test that Base assigns a unique ID
+        """
         base1 = Base()
         base2 = Base()
         self.assertEqual(base1.id, 1)
