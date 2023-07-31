919cf292def446eda96d1a246aeda6e30ce16c6b
BryanField15 <6367@holbertonstudents.com>
2023-07-31 02:59:13 -0700

    used format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""takes in an argument and displays all values in the states"""
-"""table of hbtn_0e_0_usa where name matches the argument."""
 
 import MySQLdb
 import sys
+
+"""script that takes in an argument and displays all values in the states"""
+"""table of hbtn_0e_0_usa where name matches the argument."""
 
 
 if __name__ == "__main__":
@@ -13,14 +14,14 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-
         port=3306)
 
     name = sys.argv[4]
+    query = "SELECT * FROM states WHERE BINARY name \
+        LIKE '{}%' ORDER BY id;" .format(name)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-    WHERE BINARY name LIKE %s ORDER BY id;", (name+'%',))
+    cur.execute(query)
 
     rows = cur.fetchall()
     for row in rows:
