6f3ab49c569ddaa349d91617ba3556db410f7a7a
BryanField15 <6367@holbertonstudents.com>
2023-07-31 03:25:10 -0700

    removed the %

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         LIKE %s ORDER BY id;"
 
     cur = db.cursor()
-    cur.execute(query, (sys.argv[4] + '%',))
+    cur.execute(query, (sys.argv[4],))
 
     rows = cur.fetchall()
     for row in rows:
