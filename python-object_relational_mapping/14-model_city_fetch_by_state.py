2c891f5a7c810e03a60ca658145600ee1a3c9e4f
BryanField15 <6367@holbertonstudents.com>
2023-08-01 03:09:42 -0700

    huh

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     cities_to_print = session.query(City, State).
-    filter(State.id == City.state_id).all()
+        filter(State.id == City.state_id).all()
     for city, state in cities_to_print:
         print(f"{state.name}: ({city.id}) {city.name}")
     session.commit()
